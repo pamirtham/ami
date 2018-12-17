2ede1d58203e348ae6e2676ada2ff4514f1ec1fd
pamirtham <45784364+pamirtham@users.noreply.github.com>
2018-12-17 13:59:06 +0530

    Update alphabet.py

--- alphabet.py
+++ alphabet.py
@@ -1,5 +1,5 @@
 ch = input("Enter a charecter:")
-if ((ch>='a' and ch<='z') or (ch>='A' and ch<='z')):
+if ((ch>='a' and ch<='z') or (ch>='A' and ch<='Z')):
   print(ch, "is an alphabet")
 else
   print(ch, "is not an alphabet")
