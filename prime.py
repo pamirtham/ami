00f0cd72b84c6c5936f8ae0533bed165d91eadee
pamirtham <45784364+pamirtham@users.noreply.github.com>
2018-12-18 11:12:09 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,6 +1,6 @@
 if num > 1:
     for i in range(2,num):
-        if(num % i) == 0:
+        if (num % i) == 0:
           print(num,"is not a prime number")
           print(i,"time,"num//i,"is",num)
           break
