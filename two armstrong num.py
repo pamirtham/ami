86e3d3d8272fd681bda7f7eef26e40a16bfce878
pamirtham <45784364+pamirtham@users.noreply.github.com>
2018-12-18 12:29:42 +0530

    Update two armstrong num.py

--- two armstrong num.py
+++ two armstrong num.py
@@ -2,6 +2,7 @@ lower = 100
 upper = 2000
 
 for num in range(lower,upper + 1):
+  
   order = len(str(num))
   
   sum = 0
@@ -11,5 +12,6 @@ for num in range(lower,upper + 1):
       digit = temp % 10
       sum +=digit ** order
       temp //= 10
+      
   if num == sum:
       print(num)
