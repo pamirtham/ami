514036c5c4e04ac1389588ec2c48fd4f89391130
pamirtham <45784364+pamirtham@users.noreply.github.com>
2018-12-18 13:27:44 +0530

    Update arithmetic.py

--- arithmetic.py
+++ arithmetic.py
@@ -3,7 +3,7 @@ using namespace std;
 float sumOfAP(float a, float d, int n) 
 { 
     float sum = 0; 
-    for (int i=0;i<n;i++) 
+    for (int i=0;i<n; i++) 
     { 
         sum = sum + a; 
         a = a + d; 
