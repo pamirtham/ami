018d2a3ec2e9f95f74f9c88a1ca441577269b7a7
pamirtham <45784364+pamirtham@users.noreply.github.com>
2018-12-18 12:02:17 +0530

    Update prime num.py

--- prime num.py
+++ prime num.py
@@ -7,5 +7,5 @@ for num in range(lower,upper + 1):
         for i in range(2,num):
             if (num % i) == 0:
                 break
-    else:
-        print(num)
+        else:
+            print(num)
