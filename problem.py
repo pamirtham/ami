aa4f8edc92277c83d8bf88cc05e732ee12a6dd2e
pamirtham <45784364+pamirtham@users.noreply.github.com>
2018-12-18 14:53:41 +0530

    Update problem.py

--- problem.py
+++ problem.py
@@ -1,5 +1,5 @@
 def mergeSort(alist):
-    print ("Splitting ",alist)
+    print("Splitting ",alist)
     if len(alist)>1:
         mid = len(alist)//2
         lefthalf = alist[:mid]
@@ -29,8 +29,8 @@ def mergeSort(alist):
             alist[k]=righthalf[j]
             j=j+1
             k=k+1
-    print ("Merging ",alist)
+    print("Merging ",alist)
 
 alist = [54,26,93,17,77,31,44,55,20]
 mergeSort(alist)
-print (alist)
+print(alist)
