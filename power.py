74fe359f08e1b0438f2af9493b17f5430cf3888b
pamirtham <45784364+pamirtham@users.noreply.github.com>
2018-12-18 10:26:06 +0530

    Update power.py

--- power.py
+++ power.py
@@ -1,4 +1,4 @@
 squares = 5
-result = list(map(lambda X: 2 ** X, range(terms)))
+result = list(map(lambda x: 2 ** x, range(terms)))
 for i in range(squares):
  print("2 raised to the power of",i,"is",result[i])
