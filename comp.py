2efb9a56d7c1cb6d9668511818975bfa7cd70c0a
pamirtham <45784364+pamirtham@users.noreply.github.com>
2019-01-25 15:39:40 +0530

    Update comp.py

--- comp.py
+++ comp.py
@@ -6,7 +6,7 @@ else:
     string2 = input("Enter second string: ")
     if string1 == string2:
         print("\nBoth strings are equal to each other.")
-        print(string1,"==",string2);
+        print(string1,"==",string2)
     else:
         print("\nStrings are not equal.")
-        print(string1,"!=",string2);
+        print(string1,"!=",string2)
