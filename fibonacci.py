e3febc833fc61d052285884259946a065d4d1596
pamirtham <45784364+pamirtham@users.noreply.github.com>
2019-01-25 14:16:49 +0530

    Create fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,9 @@
+if nterms <= 0:
+   print("Please enter a positive integer")
+elif nterms == 1:
+   print("Fibonacci sequence upto",nterms,":")
+   print(n1)
+else:
+   print("Fibonacci sequence upto",nterms,":")
+   while count < nterms:
+       print(n1,end=' , ')
